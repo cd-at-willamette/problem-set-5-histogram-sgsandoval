6a8c6aeebe1960f5ae899905775658d47c718a04
sgsandoval <sgsandoval@willamette.edu>
2024-10-30 08:28:26 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -2,20 +2,42 @@ from pgl import *
 
 #1a
 def create_histogram_array(data:list[int])->list[int]:
+    max_value = max(data) if data else 0
+    histogram = [0] * (max_value + 1)
+    for number in data:
+        histogram[number] += 1
+    return histogram
     pass
 
 #1b
 def print_histogram(hist:list[int]) -> None:
+    for index, value in enumerate(hist):
+        print(f"{index}: {'*' * value}")
     pass
 
 #1c
 def graph_histogram(hist:list[int], width:int, height:int) -> None:
+    gw = GWindow(width,height)
 
     def my_rect(x,y,w,h,color):
         rect = GRect(x,y,w,h)
         rect.set_filled(True)
         rect.set_color(color)
         gw.add(rect)
+
+    max_value = max(hist) if hist else 1
+    w = width / len(hist)
+
+    for i, value in enumerate(hist):
+        h = (value / max_value) * height
+
+        x = i * w
+        y = height - h
+
+    gw.add(my_rect(x,y,w,h,"Blue"))
+
+    
+
 
     pass
 
