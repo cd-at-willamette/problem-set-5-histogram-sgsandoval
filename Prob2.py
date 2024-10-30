4536a307002ef446ca7762d495703400855ae580
sgsandoval <sgsandoval@willamette.edu>
2024-10-30 08:28:52 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,12 +1,31 @@
 ##################################################
-# Name:
+# Name: Sonora
 # Collaborators:
 # Estimate of time spent (hr):
 ##################################################
 
-def is_magic_square(array:list[list[int]]) -> bool:
+def is_magic_square(square:list[list[int]]) -> bool:
+    size = len(square)
+    if any(len(row) != size for row in square):
+        return False
+    
+    target_sum = sum(square[0])
+
+    for row in square:
+        if sum(row) != target_sum:
+            return False
+        
+    for col in range(size):
+        if sum(square[row][col] for row in range(size)) != target_sum:
+            return False
+        if sum(square[i][i] for i in range(size)) != target_sum:
+            return False
+        if sum(square[i][size - 1 - i] for i in range(size)) != target_sum:
+            return False
+        return True
+    
     pass
 
-small = [[8,1,6],[3,5,7],[4,9,2]]
-print(is_magic_square(small))
+magic_sqaure = [[8,1,6],[3,5,7],[4,9,2]]
+print(is_magic_square(magic_sqaure))
 
